a4e3f061cd1217d9c409c0bc53d91ebaceb7fdd0
Aleksandar Babic <aleksandar.babic@comtradegaming.com>
2024-10-01 13:50:18 +0200

    created and dockerized example_dotnet_app

---
+++ 02_docker/example_dotnet_app/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("####################################");
+Console.WriteLine("###       Hello, Everyone!       ###");
+Console.WriteLine("### My name is Aleksandar Babic! ###");
+Console.WriteLine("####################################");
